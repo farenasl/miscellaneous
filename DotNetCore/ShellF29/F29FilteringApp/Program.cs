b3205b7cf17bb3321ba01a6942b9efb4e122ed12
BCI MacBook Pro <farenasl@MacBook-Pro-BCI.local>
2020-05-31 16:20:43 -0400

    Initial commit

---
+++ DotNetCore/ShellF29/F29FilteringApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace F29FilteringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
