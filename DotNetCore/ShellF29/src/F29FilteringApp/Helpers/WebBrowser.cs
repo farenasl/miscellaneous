fec2d6ce074f7ecc2c17bc8d6d76546fd55150ee
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-08 22:07:40 -0400

    Adding web browser navigation support

---
+++ DotNetCore/ShellF29/src/F29FilteringApp/Helpers/WebBrowser.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace F29FilteringApp.helpers
+{
+    public class WebBrowser
+    {
+        public static void OpenBrowser(string url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    System.Diagnostics.Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    System.Diagnostics.Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    System.Diagnostics.Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
--- DotNetCore/ShellF29/src/F29FilteringApp/Program.cs
+++ DotNetCore/ShellF29/src/F29FilteringApp/Program.cs
@@ -14,46 +14,28 @@ namespace F29FilteringApp
             List<Information> infoLst, filteredList;
             List<Rut> rutLst;
 
+            
+
             try
             {
+                WebBrowser.OpenBrowser("https://www.google.cl/");
                 DateTime startTime = DateTime.Now;
-                infoLst = Process.readInformationFile("../../TestData/25-regs.txt");
-                Console.WriteLine("File with " + infoLst.Count + " rows parsed in " + (DateTime.Now - startTime).TotalSeconds + " seconds");
+                // infoLst = Process.readInformationFile("../../TestData/25-regs.txt");
+                infoLst = Process.readInformationFile("../../TestData/IFIN016202004F29.txt");
+                Console.WriteLine("Incoming text file with " + infoLst.Count + " valid rows parsed in " + (DateTime.Now - startTime).TotalSeconds + " seconds");
 
                 startTime = DateTime.Now;
                 rutLst = Process.readRutFilteringFile("../../TestData/ruts.txt");
-                Console.WriteLine("File with " + rutLst.Count + " ruts parsed in " + (DateTime.Now - startTime).TotalSeconds + " seconds");
+                Console.WriteLine("Incoming excel file with " + rutLst.Count + " valid ruts parsed in " + (DateTime.Now - startTime).TotalSeconds + " seconds");
 
                 startTime = DateTime.Now;
                 filteredList = Process.filterInformation(infoLst, rutLst);
-                // startTime = DateTime.Now;
-                // infoLst = Process.readInformationFile("TestData/IFIN016202004F29_50K_7.txt");
-                // Console.WriteLine("File with " + infoLst.Count + " rows parsed in " + (DateTime.Now - startTime).TotalSeconds + "seconds");
+                Console.WriteLine("Outcoming text file with " + filteredList.Count + " valid rows parsed in " + (DateTime.Now - startTime).TotalSeconds + " seconds");
 
-                // startTime = DateTime.Now;
-                // infoLst = Process.readInformationFile("TestData/IFIN016202004F29_50K_1.txt");
-                // Console.WriteLine("File with " + infoLst.Count + " rows parsed in " + (DateTime.Now - startTime).TotalSeconds + "seconds");
-
-                // startTime = DateTime.Now;
-                // infoLst = Process.readInformationFile("TestData/IFIN016202004F29_100K_1.txt");
-                // Console.WriteLine("File with " + infoLst.Count + " rows parsed in " + (DateTime.Now - startTime).TotalSeconds + "seconds");
-
-                // startTime = DateTime.Now;
-                // infoLst = Process.readInformationFile("TestData/IFIN016202004F29_NO_EMPRESAS.txt");
-                // Console.WriteLine("File with " + infoLst.Count + " rows parsed in " + (DateTime.Now - startTime).TotalSeconds + "seconds");
-
-                // startTime = DateTime.Now;
-                // infoLst = Process.readInformationFile("TestData/IFIN016202004F29_EMPRESAS.txt");
-                // Console.WriteLine("File with " + infoLst.Count + " rows parsed in " + (DateTime.Now - startTime).TotalSeconds + "seconds");
-
-                // startTime = DateTime.Now;
-                // infoLst = Process.readInformationFile("TestData/IFIN016202004F29.txt");
-                // Console.WriteLine("File with " + infoLst.Count + " rows parsed in " + (DateTime.Now - startTime).TotalSeconds + "seconds");
-            
                 // infoLst.ForEach(Console.WriteLine);
                 // rutLst.ForEach(Console.WriteLine);
-                filteredList.ForEach(Console.WriteLine);
-                Console.WriteLine(filteredList.Count);
+                //filteredList.ForEach(Console.WriteLine);
+                Console.WriteLine("Data rows generated: " + filteredList.Count);
             }
             catch (IOException e)
             {
