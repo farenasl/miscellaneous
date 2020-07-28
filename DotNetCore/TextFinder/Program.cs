b0f7a12db518fde84f316f621c9a5916f54a4fea
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-27 21:33:29 -0400

    Project creation

---
+++ DotNetCore/TextFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
