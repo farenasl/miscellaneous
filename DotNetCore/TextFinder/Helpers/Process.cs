25a15f54e27d1054234826cf0cf945f682ca5b94
BCI MacBook Pro <farenasl@NBADMCBCXB9.local>
2020-07-27 22:38:23 -0400

    generating filtered list

--- DotNetCore/TextFinder/Helpers/Process.cs
+++ DotNetCore/TextFinder/Helpers/Process.cs
@@ -21,14 +21,25 @@ namespace TextFinder.helpers
         }
 
         public static void filterInformation(List<String> lst) {
+            List<String> finalLst = new List<String>();
             try
             {
                 lst.RemoveAll(row => row.Split(" ") == null || row.Split(" ").Length < 3);
                 lst.RemoveAll(row => row.Split(" ")[2] != "ERROR");
 
-                
+                foreach (var item in lst)
+                {
+                    var element = item.Split(" ");
+                    if (element[8].Equals("[DataMartClient]")){
+                        if (element[9].Equals("[generarSuscripcion]"))
+                            finalLst.Add("ERROR GENERANDO SUBSCRIPCION: " + element[6] + " --- " + element[0] + " " + element[1]);
+                        else if (element[9].Equals("[verificarSuscripcion]"))
+                            finalLst.Add("ERROR VERIFICANDO SUBSCRIPCION: " + element[6] + " --- " + element[0] + " " + element[1]);
+                    } 
+                }
 
                 Console.WriteLine("Logradas: " + lst.Count.ToString());
+                Console.WriteLine("Errores: " + finalLst.Count.ToString());
             }
             catch (System.Exception ex)
             {
@@ -36,7 +47,7 @@ namespace TextFinder.helpers
                 throw ex;
             }
             finally {
-                lst.ForEach(Console.WriteLine);
+                finalLst.ForEach(Console.WriteLine);
             }
         }
     }
