f1e2334b4bd3b63d65068fe5b5704c0af917e707
farenasl <54385555+farenasl@users.noreply.github.com>
2023-02-16 22:29:36 -0300

    Add files via upload

--- Java/azureappconfigconnection/src/test/java/com/example/azureappconfigconnection/AzureAppConfigConnectionApplicationTests.java
+++ Java/azureappconfigconnection/src/test/java/com/example/azureappconfigconnection/AzureAppConfigConnectionApplicationTests.java
@@ -1,1 +1,13 @@
+package com.example.azureappconfigconnection;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureAppConfigConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
