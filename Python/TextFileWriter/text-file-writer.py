04a7c8ec75f3a048da9248a73088b1f207ccbd6b
farenasl <fabian.arenas.l@gmail.com>
2023-11-21 14:40:40 -0300

    Adding time tracking for the execution

--- Python/TextFileWriter/text-file-writer.py
+++ Python/TextFileWriter/text-file-writer.py
@@ -1,3 +1,8 @@
+import time
+
+# get the start time
+st = time.time()
+
 f = open("simple-text-file.txt", "w")
 
 for x in range(1,100001):
@@ -8,3 +13,9 @@ f.close()
 #open and read the file after the appending:
 #f = open("simple-text-file.txt", "r")
 #print(f.read())
+
+# get the end time
+et = time.time()
+
+# get the execution time
+print('Script executed in:', et - st, 'seconds')
