ffd096c9fef63dcfce29d0eca74d54f23e9e7766
farenasl <fabian.arenas.l@gmail.com>
2025-04-02 16:24:53 -0300

    RAC: Refactor to process all diferent files

--- DotNetCore/ReadinessAssessmentConsolidator/Program.cs
+++ DotNetCore/ReadinessAssessmentConsolidator/Program.cs
@@ -7,7 +7,9 @@ namespace ReadinessAssessmentConsolidator
 {
     class Program
     {
-        static void Main(string[] args)
+        private static List<ExcelDataModel> feedback = new();
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Starting the Readiness Assessment Consolidator program!!");
 
@@ -18,175 +20,59 @@ namespace ReadinessAssessmentConsolidator
             Console.WriteLine("We have found " + files.Length + " Excel files to process");
             // Array.ForEach(files, Console.WriteLine);
 
-            Array.ForEach(files, ProcessExcelData);
-
-            string filePath = "excelFiles/BMA Technology Readiness Survey - Fabian Arenas.xlsx";
-
-            int rowCounter = 1;
-
-            List<ExcelDataModel> feedback = new();
-
-            using (var workbook = new XLWorkbook(filePath)){
-                var worksheet = workbook.Worksheet("BNE DC");
-
-                //foreach (var row in worksheet.Rows())
-                foreach (var row in worksheet.RowsUsed().Skip(4))
-                {
-                    if (feedback.Count < 89)
-                        feedback.Add(new ExcelDataModel()
-                            {
-                                UserEstimation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString(),
-                                UserJustification = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString(),
-                                UserRecommendation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString()
-                            }
-                        );
-                    else {
-                        feedback[rowCounter].UserEstimation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString();
-                        feedback[rowCounter].UserJustification += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString();
-                        feedback[rowCounter].UserRecommendation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString();
-                    }
-
-                    rowCounter++;
-
-                    if (rowCounter > 92) //92 is the limit of the info, this can be an arg
-                        break;
-                }
-            }
-
-            filePath = "excelFiles/BMA Technology Readiness Survey - Nathan Young.xlsx";
-            rowCounter = 1;
-
-            using (var workbook = new XLWorkbook(filePath)){
-                var worksheet = workbook.Worksheet("BNE DC");
-
-                //foreach (var row in worksheet.Rows())
-                foreach (var row in worksheet.RowsUsed().Skip(4))
-                {
-                    if (feedback.Count == 0)
-                        feedback.Add(new ExcelDataModel()
-                            {
-                                UserEstimation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString(),
-                                UserJustification = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString(),
-                                UserRecommendation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString()
-                            }
-                        );
-                    else {
-                        feedback[rowCounter - 1].UserEstimation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString();
-                        feedback[rowCounter - 1].UserJustification += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString();
-                        feedback[rowCounter - 1].UserRecommendation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString();
-                    }
-
-                    rowCounter++;
-
-                    if (rowCounter > 92) //92 is the limit of the info, this can be an arg
-                        break;
-                }
-            }
-
-            filePath = "excelFiles/BMA Technology Readiness Survey - Michelle Fernando.xlsx";
-            rowCounter = 1;
-
-            using (var workbook = new XLWorkbook(filePath)){
-                var worksheet = workbook.Worksheet("BNE DC");
-
-                //foreach (var row in worksheet.Rows())
-                foreach (var row in worksheet.RowsUsed().Skip(4))
-                {
-                    if (feedback.Count == 0)
-                        feedback.Add(new ExcelDataModel()
-                            {
-                                UserEstimation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString(),
-                                UserJustification = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString(),
-                                UserRecommendation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString()
-                            }
-                        );
-                    else {
-                        feedback[rowCounter - 1].UserEstimation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString();
-                        feedback[rowCounter - 1].UserJustification += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString();
-                        feedback[rowCounter - 1].UserRecommendation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString();
-                    }
-
-                    rowCounter++;
-
-                    if (rowCounter > 92) //92 is the limit of the info, this can be an arg
-                        break;
-                }
-            }
-
-            filePath = "excelFiles/BMA Technology Readiness Survey - Eddie Pearson.xlsx";
-            rowCounter = 1;
-
-            using (var workbook = new XLWorkbook(filePath)){
-                var worksheet = workbook.Worksheet("BNE DC");
-
-                //foreach (var row in worksheet.Rows())
-                foreach (var row in worksheet.RowsUsed().Skip(4))
-                {
-                    if (feedback.Count == 0)
-                        feedback.Add(new ExcelDataModel()
-                            {
-                                UserEstimation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString(),
-                                UserJustification = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString(),
-                                UserRecommendation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString()
-                            }
-                        );
-                    else {
-                        feedback[rowCounter - 1].UserEstimation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString();
-                        feedback[rowCounter - 1].UserJustification += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString();
-                        feedback[rowCounter - 1].UserRecommendation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString();
-                    }
-
-                    rowCounter++;
-
-                    if (rowCounter > 92) //92 is the limit of the info, this can be an arg
-                        break;
-                }
-            }
-
-            filePath = "excelFiles/BMA Technology Readiness Survey - Chris Groves.xlsx";
-            rowCounter = 1;
-
-            using (var workbook = new XLWorkbook(filePath)){
-                var worksheet = workbook.Worksheet("BNE DC");
-
-                //foreach (var row in worksheet.Rows())
-                foreach (var row in worksheet.RowsUsed().Skip(4))
-                {
-                    if (feedback.Count == 0)
-                        feedback.Add(new ExcelDataModel()
-                            {
-                                UserEstimation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString(),
-                                UserJustification = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString(),
-                                UserRecommendation = filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString()
-                            }
-                        );
-                    else {
-                        feedback[rowCounter - 1].UserEstimation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(3).GetString();
-                        feedback[rowCounter - 1].UserJustification += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(4).GetString();
-                        feedback[rowCounter - 1].UserRecommendation += "\n" + filePath.Split(" - ")[1].Replace(".xlsx", string.Empty) + ": " + row.Cell(5).GetString();
-                    }
-
-                    rowCounter++;
-
-                    if (rowCounter > 92) //92 is the limit of the info, this can be an arg
-                        break;
-                }
-            }
+            Array.ForEach(files, ProcessExcel);
 
             //feedback.ForEach(i => Console.Write("{0}\n", i));
 
-            var wb = new XLWorkbook();
-            var ws = wb.Worksheets.Add("BNE DC");
-            ws.Cell(5, 3).InsertData(feedback);
-            // wb.SaveAs("excelFiles/BMA Technology Readiness Survey - Consolidated.xlsx");
-            wb.SaveAs("excelFiles/BMA_Technology_Readiness_Survey_Consolidated.xlsx");
+            SavingConsolidatedExcel();
 
             Console.WriteLine("Ending the Readiness Assessment Consolidator program!!");
         }
 
-        private static void ProcessExcelData(string file){
-            Console.WriteLine("Starting processing file " + file);
-            Console.WriteLine("End processing file " + file);
+        private static void ProcessExcel(String filePath){
+            Console.WriteLine("Starting processing file " + filePath);
+
+            using (var workbook = new XLWorkbook(filePath)){
+                ProcessFeedback(filePath.Split(" - ")[1].Replace(".xlsx", string.Empty), workbook.Worksheet("BNE DC"));
+            }
+            Console.WriteLine("End processing file " + filePath);
+        }
+
+        private static void ProcessFeedback(String associatedUser, IXLWorksheet worksheet){
+            if (feedback.Count < 79)
+                ProcessingFirstFile(associatedUser, worksheet);
+            else {
+                ProcessingNotFirstFile(associatedUser, worksheet);
+            }
+        }
+
+        private static void ProcessingFirstFile(String associatedUser, IXLWorksheet worksheet) {
+            foreach (var row in worksheet.RowsUsed().Skip(4))
+                feedback.Add(new ExcelDataModel()
+                    {
+                        UserEstimation = associatedUser + ": " + row.Cell(3).GetString(),
+                        UserJustification = associatedUser + ": " + row.Cell(4).GetString(),
+                        UserRecommendation = associatedUser + ": " + row.Cell(5).GetString()
+                    }
+                );
+        }
+
+        private static void ProcessingNotFirstFile(String associatedUser, IXLWorksheet worksheet) {
+            int rowCounter = 0;
+            foreach (var row in worksheet.RowsUsed().Skip(4)) {
+                feedback[rowCounter].UserEstimation += "\n" + associatedUser + ": " + row.Cell(3).GetString();
+                feedback[rowCounter].UserJustification += "\n" + associatedUser + ": " + row.Cell(4).GetString();
+                feedback[rowCounter].UserRecommendation += "\n" + associatedUser + ": " + row.Cell(5).GetString();
+                rowCounter++;
+            }
+        }
+
+        private static void SavingConsolidatedExcel() {
+            var wb = new XLWorkbook();
+            var ws = wb.Worksheets.Add("BNE DC");
+            ws.Cell(5, 3).InsertData(feedback);
+            // wb.SaveAs("excelFiles/BMA Technology Readiness Survey - Consolidated.xlsx");
+            wb.SaveAs("results/BMA_Technology_Readiness_Survey_Consolidated_" + DateTime.Now.ToString("yyyyMMdd_HHmmss")+ ".xlsx");
         }
     }
 }
