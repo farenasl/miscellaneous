052a7804c78c3042ff247d8989cfade314d25802
farenasl <fabian.arenas.l@gmail.com>
2025-04-02 14:43:15 -0300

    RAC: Setting objects  as nullables

--- DotNetCore/ReadinessAssessmentConsolidator/Models/ExcelDataModel.cs
+++ DotNetCore/ReadinessAssessmentConsolidator/Models/ExcelDataModel.cs
@@ -3,9 +3,9 @@ namespace ReadinessAssessmentConsolidator.models
 {
     public class ExcelDataModel
     {
-        public String UserEstimation { get; set; }
-        public String UserJustification { get; set; }
-        public String UserRecommendation { get; set; }
+        public String? UserEstimation { get; set; }
+        public String? UserJustification { get; set; }
+        public String? UserRecommendation { get; set; }
 
         public override string ToString() {
             return UserEstimation + "\n" + UserJustification + "\n" + UserRecommendation;
