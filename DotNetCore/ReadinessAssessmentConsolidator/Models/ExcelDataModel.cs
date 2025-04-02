83c5b9da28860a06790615b2936d82e0d6392b6d
farenasl <fabian.arenas.l@gmail.com>
2025-04-02 16:28:48 -0300

    RAC: Formatting files

--- DotNetCore/ReadinessAssessmentConsolidator/Models/ExcelDataModel.cs
+++ DotNetCore/ReadinessAssessmentConsolidator/Models/ExcelDataModel.cs
@@ -7,7 +7,8 @@ namespace ReadinessAssessmentConsolidator.models
         public String? UserJustification { get; set; }
         public String? UserRecommendation { get; set; }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return UserEstimation + "\n" + UserJustification + "\n" + UserRecommendation;
         }
     }
--- DotNetCore/ReadinessAssessmentConsolidator/Program.cs
+++ DotNetCore/ReadinessAssessmentConsolidator/Program.cs
@@ -12,11 +12,11 @@ namespace ReadinessAssessmentConsolidator
         private static void Main(string[] args)
         {
             Console.WriteLine("Starting the Readiness Assessment Consolidator program!!");
-
             Console.WriteLine("Reading all excels inside of excelFiles folder");
             Console.WriteLine("Only files with the extension XLSX will be considered");
 
             string[] files = Directory.GetFiles("excelFiles/", "*.xlsx");
+
             Console.WriteLine("We have found " + files.Length + " Excel files to process");
             // Array.ForEach(files, Console.WriteLine);
 
@@ -29,37 +29,43 @@ namespace ReadinessAssessmentConsolidator
             Console.WriteLine("Ending the Readiness Assessment Consolidator program!!");
         }
 
-        private static void ProcessExcel(String filePath){
+        private static void ProcessExcel(String filePath)
+        {
             Console.WriteLine("Starting processing file " + filePath);
 
-            using (var workbook = new XLWorkbook(filePath)){
+            using (var workbook = new XLWorkbook(filePath))
+            {
                 ProcessFeedback(filePath.Split(" - ")[1].Replace(".xlsx", string.Empty), workbook.Worksheet("BNE DC"));
             }
+
             Console.WriteLine("End processing file " + filePath);
         }
 
-        private static void ProcessFeedback(String associatedUser, IXLWorksheet worksheet){
+        private static void ProcessFeedback(String associatedUser, IXLWorksheet worksheet)
+        {
             if (feedback.Count < 79)
                 ProcessingFirstFile(associatedUser, worksheet);
-            else {
+            else
                 ProcessingNotFirstFile(associatedUser, worksheet);
-            }
         }
 
-        private static void ProcessingFirstFile(String associatedUser, IXLWorksheet worksheet) {
+        private static void ProcessingFirstFile(String associatedUser, IXLWorksheet worksheet)
+        {
             foreach (var row in worksheet.RowsUsed().Skip(4))
                 feedback.Add(new ExcelDataModel()
-                    {
-                        UserEstimation = associatedUser + ": " + row.Cell(3).GetString(),
-                        UserJustification = associatedUser + ": " + row.Cell(4).GetString(),
-                        UserRecommendation = associatedUser + ": " + row.Cell(5).GetString()
-                    }
+                {
+                    UserEstimation = associatedUser + ": " + row.Cell(3).GetString(),
+                    UserJustification = associatedUser + ": " + row.Cell(4).GetString(),
+                    UserRecommendation = associatedUser + ": " + row.Cell(5).GetString()
+                }
                 );
         }
 
-        private static void ProcessingNotFirstFile(String associatedUser, IXLWorksheet worksheet) {
+        private static void ProcessingNotFirstFile(String associatedUser, IXLWorksheet worksheet)
+        {
             int rowCounter = 0;
-            foreach (var row in worksheet.RowsUsed().Skip(4)) {
+            foreach (var row in worksheet.RowsUsed().Skip(4))
+            {
                 feedback[rowCounter].UserEstimation += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                 feedback[rowCounter].UserJustification += "\n" + associatedUser + ": " + row.Cell(4).GetString();
                 feedback[rowCounter].UserRecommendation += "\n" + associatedUser + ": " + row.Cell(5).GetString();
@@ -67,12 +73,13 @@ namespace ReadinessAssessmentConsolidator
             }
         }
 
-        private static void SavingConsolidatedExcel() {
+        private static void SavingConsolidatedExcel()
+        {
             var wb = new XLWorkbook();
             var ws = wb.Worksheets.Add("BNE DC");
             ws.Cell(5, 3).InsertData(feedback);
             // wb.SaveAs("excelFiles/BMA Technology Readiness Survey - Consolidated.xlsx");
-            wb.SaveAs("results/BMA_Technology_Readiness_Survey_Consolidated_" + DateTime.Now.ToString("yyyyMMdd_HHmmss")+ ".xlsx");
+            wb.SaveAs("results/BMA_Technology_Readiness_Survey_Consolidated_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".xlsx");
         }
     }
 }
