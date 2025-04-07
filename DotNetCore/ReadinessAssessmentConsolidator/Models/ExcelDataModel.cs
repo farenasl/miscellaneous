2b40bab97416db3120dafae4d92f2f315dfcc276
farenasl <fabian.arenas.l@gmail.com>
2025-04-07 13:28:13 -0400

    RAC: Adjusting datamodel to handle all columns

--- DotNetCore/ReadinessAssessmentConsolidator/Models/ExcelDataModel.cs
+++ DotNetCore/ReadinessAssessmentConsolidator/Models/ExcelDataModel.cs
@@ -3,13 +3,15 @@ namespace ReadinessAssessmentConsolidator.models
 {
     public class ExcelDataModel
     {
-        public String? UserEstimation { get; set; }
-        public String? UserJustification { get; set; }
-        public String? UserRecommendation { get; set; }
+        public String? DataColumn1 { get; set; }
+        public String? DataColumn2 { get; set; }
+        public String? DataColumn3 { get; set; }
+        public String? DataColumn4 { get; set; }
+        public String? DataColumn5 { get; set; }
 
         public override string ToString()
         {
-            return UserEstimation + "\n" + UserJustification + "\n" + UserRecommendation;
+            return DataColumn3 + "\n" + DataColumn4 + "\n" + DataColumn5;
         }
     }
 }
