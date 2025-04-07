176f38c3e2586486d89e9c1113904390f7f0a8ad
farenasl <fabian.arenas.l@gmail.com>
2025-04-07 18:47:37 -0400

    RAC: Solved issue about missing or overlapped rows in results workbook

--- DotNetCore/ReadinessAssessmentConsolidator/Program.cs
+++ DotNetCore/ReadinessAssessmentConsolidator/Program.cs
@@ -275,7 +275,7 @@ namespace ReadinessAssessmentConsolidator
 
         private static void ProcessingFirstFile(String associatedUser, IXLWorksheet worksheet)
         {
-            foreach (var row in worksheet.RowsUsed().Skip(4))
+            foreach (var row in worksheet.Rows().Skip(4))
                 switch (worksheet.Name)
                 {
                     case "BNE DC":
@@ -284,7 +284,8 @@ namespace ReadinessAssessmentConsolidator
                         || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
                         || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
                         || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
-                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86)
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 93)
                             feedback1.Add(new ExcelDataModel());
                         else if (row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
                             || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
@@ -310,7 +311,8 @@ namespace ReadinessAssessmentConsolidator
                         || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
                         || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
                         || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
-                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86)
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 93)
                             feedback2.Add(new ExcelDataModel());
                         else if (row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
                             || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
@@ -336,7 +338,8 @@ namespace ReadinessAssessmentConsolidator
                         || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
                         || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
                         || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
-                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86)
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 93)
                             feedback3.Add(new ExcelDataModel());
                         else if (row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
                             || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
@@ -362,7 +365,8 @@ namespace ReadinessAssessmentConsolidator
                         || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
                         || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
                         || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
-                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86)
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 93)
                             feedback4.Add(new ExcelDataModel());
                         else if (row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
                             || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
@@ -388,7 +392,8 @@ namespace ReadinessAssessmentConsolidator
                         || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
                         || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
                         || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
-                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86)
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 93)
                             feedback5.Add(new ExcelDataModel());
                         else if (row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
                             || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
@@ -414,7 +419,8 @@ namespace ReadinessAssessmentConsolidator
                         || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
                         || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
                         || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
-                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86)
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 93)
                             feedback6.Add(new ExcelDataModel());
                         else if (row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
                             || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
@@ -440,7 +446,8 @@ namespace ReadinessAssessmentConsolidator
                         || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
                         || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
                         || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
-                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86)
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 93)
                             feedback7.Add(new ExcelDataModel());
                         else if (row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
                             || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
@@ -471,10 +478,22 @@ namespace ReadinessAssessmentConsolidator
             switch (worksheet.Name)
             {
                 case "BNE DC":
-                    foreach (var row in worksheet.RowsUsed().Skip(4))
+                    foreach (var row in worksheet.Rows().Skip(4))
                     {
-                        if (rowCounter == 82)
-                            break;
+                        if (rowCounter == 93 || row.RowNumber() == 11 || row.RowNumber() == 13 || row.RowNumber() == 18
+                        || row.RowNumber() == 20 || row.RowNumber() == 25 || row.RowNumber() == 27
+                        || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
+                        || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
+                        || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
+                        || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
+                        || row.RowNumber() == 65 || row.RowNumber() == 75 || row.RowNumber() == 85
+                        || row.RowNumber() == 93)
+                        {
+                            rowCounter++;
+                            continue;
+                        }
 
                         feedback1[rowCounter].DataColumn3 += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                         feedback1[rowCounter].DataColumn4 += "\n" + associatedUser + ": " + row.Cell(4).GetString();
@@ -483,10 +502,22 @@ namespace ReadinessAssessmentConsolidator
                     }
                     break;
                 case "GOONYELLA":
-                    foreach (var row in worksheet.RowsUsed().Skip(4))
+                    foreach (var row in worksheet.Rows().Skip(4))
                     {
-                        if (rowCounter == 82)
-                            break;
+                        if (rowCounter == 93 || row.RowNumber() == 11 || row.RowNumber() == 13 || row.RowNumber() == 18
+                        || row.RowNumber() == 20 || row.RowNumber() == 25 || row.RowNumber() == 27
+                        || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
+                        || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
+                        || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
+                        || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
+                        || row.RowNumber() == 65 || row.RowNumber() == 75 || row.RowNumber() == 85
+                        || row.RowNumber() == 93)
+                        {
+                            rowCounter++;
+                            continue;
+                        }
 
                         feedback2[rowCounter].DataColumn3 += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                         feedback2[rowCounter].DataColumn4 += "\n" + associatedUser + ": " + row.Cell(4).GetString();
@@ -495,10 +526,22 @@ namespace ReadinessAssessmentConsolidator
                     }
                     break;
                 case "BROADMEADOW":
-                    foreach (var row in worksheet.RowsUsed().Skip(4))
+                    foreach (var row in worksheet.Rows().Skip(4))
                     {
-                        if (rowCounter == 82)
-                            break;
+                        if (rowCounter == 93 || row.RowNumber() == 11 || row.RowNumber() == 13 || row.RowNumber() == 18
+                        || row.RowNumber() == 20 || row.RowNumber() == 25 || row.RowNumber() == 27
+                        || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
+                        || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
+                        || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
+                        || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
+                        || row.RowNumber() == 65 || row.RowNumber() == 75 || row.RowNumber() == 85
+                        || row.RowNumber() == 93)
+                        {
+                            rowCounter++;
+                            continue;
+                        }
 
                         feedback3[rowCounter].DataColumn3 += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                         feedback3[rowCounter].DataColumn4 += "\n" + associatedUser + ": " + row.Cell(4).GetString();
@@ -507,10 +550,22 @@ namespace ReadinessAssessmentConsolidator
                     }
                     break;
                 case "CAVAL RIDGE":
-                    foreach (var row in worksheet.RowsUsed().Skip(4))
+                    foreach (var row in worksheet.Rows().Skip(4))
                     {
-                        if (rowCounter == 82)
-                            break;
+                        if (rowCounter == 93 || row.RowNumber() == 11 || row.RowNumber() == 13 || row.RowNumber() == 18
+                        || row.RowNumber() == 20 || row.RowNumber() == 25 || row.RowNumber() == 27
+                        || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
+                        || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
+                        || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
+                        || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
+                        || row.RowNumber() == 65 || row.RowNumber() == 75 || row.RowNumber() == 85
+                        || row.RowNumber() == 93)
+                        {
+                            rowCounter++;
+                            continue;
+                        }
 
                         feedback4[rowCounter].DataColumn3 += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                         feedback4[rowCounter].DataColumn4 += "\n" + associatedUser + ": " + row.Cell(4).GetString();
@@ -519,10 +574,22 @@ namespace ReadinessAssessmentConsolidator
                     }
                     break;
                 case "PEAK DOWNS":
-                    foreach (var row in worksheet.RowsUsed().Skip(4))
+                    foreach (var row in worksheet.Rows().Skip(4))
                     {
-                        if (rowCounter == 82)
-                            break;
+                        if (rowCounter == 93 || row.RowNumber() == 11 || row.RowNumber() == 13 || row.RowNumber() == 18
+                        || row.RowNumber() == 20 || row.RowNumber() == 25 || row.RowNumber() == 27
+                        || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
+                        || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
+                        || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
+                        || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
+                        || row.RowNumber() == 65 || row.RowNumber() == 75 || row.RowNumber() == 85
+                        || row.RowNumber() == 93)
+                        {
+                            rowCounter++;
+                            continue;
+                        }
 
                         feedback5[rowCounter].DataColumn3 += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                         feedback5[rowCounter].DataColumn4 += "\n" + associatedUser + ": " + row.Cell(4).GetString();
@@ -531,10 +598,22 @@ namespace ReadinessAssessmentConsolidator
                     }
                     break;
                 case "SARAJI":
-                    foreach (var row in worksheet.RowsUsed().Skip(4))
+                    foreach (var row in worksheet.Rows().Skip(4))
                     {
-                        if (rowCounter == 82)
-                            break;
+                        if (rowCounter == 93 || row.RowNumber() == 11 || row.RowNumber() == 13 || row.RowNumber() == 18
+                        || row.RowNumber() == 20 || row.RowNumber() == 25 || row.RowNumber() == 27
+                        || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
+                        || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
+                        || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
+                        || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
+                        || row.RowNumber() == 65 || row.RowNumber() == 75 || row.RowNumber() == 85
+                        || row.RowNumber() == 93)
+                        {
+                            rowCounter++;
+                            continue;
+                        }
 
                         feedback6[rowCounter].DataColumn3 += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                         feedback6[rowCounter].DataColumn4 += "\n" + associatedUser + ": " + row.Cell(4).GetString();
@@ -543,10 +622,22 @@ namespace ReadinessAssessmentConsolidator
                     }
                     break;
                 case "SARAJI STH":
-                    foreach (var row in worksheet.RowsUsed().Skip(4))
+                    foreach (var row in worksheet.Rows().Skip(4))
                     {
-                        if (rowCounter == 82)
-                            break;
+                        if (rowCounter == 93 || row.RowNumber() == 11 || row.RowNumber() == 13 || row.RowNumber() == 18
+                        || row.RowNumber() == 20 || row.RowNumber() == 25 || row.RowNumber() == 27
+                        || row.RowNumber() == 33 || row.RowNumber() == 35 || row.RowNumber() == 43
+                        || row.RowNumber() == 45 || row.RowNumber() == 55 || row.RowNumber() == 57
+                        || row.RowNumber() == 64 || row.RowNumber() == 66 || row.RowNumber() == 74
+                        || row.RowNumber() == 76 || row.RowNumber() == 84 || row.RowNumber() == 86
+                        || row.RowNumber() == 12 || row.RowNumber() == 19 || row.RowNumber() == 26
+                        || row.RowNumber() == 34 || row.RowNumber() == 44 || row.RowNumber() == 56
+                        || row.RowNumber() == 65 || row.RowNumber() == 75 || row.RowNumber() == 85
+                        || row.RowNumber() == 93)
+                        {
+                            rowCounter++;
+                            continue;
+                        }
 
                         feedback7[rowCounter].DataColumn3 += "\n" + associatedUser + ": " + row.Cell(3).GetString();
                         feedback7[rowCounter].DataColumn4 += "\n" + associatedUser + ": " + row.Cell(4).GetString();
